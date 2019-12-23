eb4c373bbfb2bb310e2b6816c937183d2ad51daf
Janne Peippo <peippo@lumous.com>
2019-12-23 14:21:34 +0200

    Add osa 8b 8.8 answer

--- src/components/Authors.js
+++ src/components/Authors.js
@@ -1,31 +1,52 @@
 import React from "react";
+import { Query } from "react-apollo";
+import { gql } from "apollo-boost";
+
+const ALL_AUTHORS = gql`
+	{
+		allAuthors {
+			name
+			born
+			id
+			bookCount
+		}
+	}
+`;
 
 const Authors = props => {
 	if (!props.show) {
 		return null;
 	}
-	const authors = [];
 
 	return (
-		<div>
-			<h2>authors</h2>
-			<table>
-				<tbody>
-					<tr>
-						<th></th>
-						<th>born</th>
-						<th>books</th>
-					</tr>
-					{authors.map(a => (
-						<tr key={a.name}>
-							<td>{a.name}</td>
-							<td>{a.born}</td>
-							<td>{a.bookCount}</td>
-						</tr>
-					))}
-				</tbody>
-			</table>
-		</div>
+		<Query query={ALL_AUTHORS}>
+			{result => {
+				if (result.loading) {
+					return <div>loading...</div>;
+				}
+				return (
+					<div>
+						<h2>authors</h2>
+						<table>
+							<tbody>
+								<tr>
+									<th></th>
+									<th>born</th>
+									<th>books</th>
+								</tr>
+								{result.data.allAuthors.map(author => (
+									<tr key={author.id}>
+										<td>{author.name}</td>
+										<td>{author.born}</td>
+										<td>{author.bookCount}</td>
+									</tr>
+								))}
+							</tbody>
+						</table>
+					</div>
+				);
+			}}
+		</Query>
 	);
 };
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ApolloProvider } from "react-apollo";
 import App from "./App";
+import { ApolloClient } from "apollo-boost";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const cache = new InMemoryCache();
+const link = new HttpLink({
+	uri: "http://localhost:4000/"
+});
+
+const client = new ApolloClient({
+	cache,
+	link
+});
+
+ReactDOM.render(
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>,
+	document.getElementById("root")
+);
