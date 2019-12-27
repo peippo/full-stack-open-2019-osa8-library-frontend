c7a003b004c09bbab91eaaa9d2966054beb7ed18
Janne Peippo <peippo@lumous.com>
2019-12-27 12:16:15 +0200

    Add osa 8b 8.9 answer

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,33 +1,52 @@
 import React from "react";
+import { Query } from "react-apollo";
+import { gql } from "apollo-boost";
+
+const ALL_BOOKS = gql`
+	{
+		allBooks {
+			title
+			author
+			published
+			id
+		}
+	}
+`;
 
 const Books = props => {
 	if (!props.show) {
 		return null;
 	}
 
-	const books = [];
-
 	return (
-		<div>
-			<h2>books</h2>
-
-			<table>
-				<tbody>
-					<tr>
-						<th></th>
-						<th>author</th>
-						<th>published</th>
-					</tr>
-					{books.map(a => (
-						<tr key={a.title}>
-							<td>{a.title}</td>
-							<td>{a.author}</td>
-							<td>{a.published}</td>
-						</tr>
-					))}
-				</tbody>
-			</table>
-		</div>
+		<Query query={ALL_BOOKS}>
+			{result => {
+				if (result.loading) {
+					return <div>loading...</div>;
+				}
+				return (
+					<div>
+						<h2>books</h2>
+						<table>
+							<tbody>
+								<tr>
+									<th></th>
+									<th>author</th>
+									<th>published</th>
+								</tr>
+								{result.data.allBooks.map(book => (
+									<tr key={book.id}>
+										<td>{book.title}</td>
+										<td>{book.author}</td>
+										<td>{book.published}</td>
+									</tr>
+								))}
+							</tbody>
+						</table>
+					</div>
+				);
+			}}
+		</Query>
 	);
 };
 
