39b15c8fe244b0b47522c4d3a9429da3b055dae4
Janne Peippo <peippo@lumous.com>
2020-01-05 13:53:32 +0200

    Add osa 8c 8.19 answer

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Books = ({ result, show }) => {
+	const [genre, setGenre] = useState("");
+
 	if (!show) {
 		return null;
 	}
+
+	const uniqueGenres = [];
+	result.data.allBooks.forEach(book => {
+		book.genres.forEach(genre => {
+			if (!uniqueGenres.includes(genre)) {
+				uniqueGenres.push(genre);
+			}
+		});
+	});
 
 	if (result.loading) {
 		return <div>loading...</div>;
@@ -11,6 +22,27 @@ const Books = ({ result, show }) => {
 		return (
 			<div>
 				<h2>books</h2>
+
+				<div>
+					Filter by genre:{" "}
+					<select
+						value={genre}
+						onChange={event => setGenre(event.target.value)}
+					>
+						<option value="" disabled>
+							Select genre
+						</option>
+						{uniqueGenres.map((genre, index) => (
+							<option key={index} value={genre}>
+								{genre}
+							</option>
+						))}
+					</select>
+					{genre && (
+						<button onClick={() => setGenre("")}>Reset</button>
+					)}
+				</div>
+
 				<table>
 					<tbody>
 						<tr>
@@ -18,13 +50,18 @@ const Books = ({ result, show }) => {
 							<th>author</th>
 							<th>published</th>
 						</tr>
-						{result.data.allBooks.map(book => (
-							<tr key={book.id}>
-								<td>{book.title}</td>
-								<td>{book.author?.name}</td>
-								<td>{book.published}</td>
-							</tr>
-						))}
+						{result.data.allBooks
+							.filter(
+								book =>
+									genre === "" || book.genres.includes(genre)
+							)
+							.map(book => (
+								<tr key={book.id}>
+									<td>{book.title}</td>
+									<td>{book.author?.name}</td>
+									<td>{book.published}</td>
+								</tr>
+							))}
 					</tbody>
 				</table>
 			</div>
